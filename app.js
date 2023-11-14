8f47d28e73dfb0fd3c8967b09da369ba9f8eaea3
agniain <agnianurul28@gmail.com>
2023-11-15 05:29:50 +0700

    tanpa err

--- app.js
+++ app.js
@@ -4,11 +4,5 @@ const router = require('./routes');
 
 app.use(express.json());
 app.use(router);
-app.use((req, res, next) => {
-    res.status(404);
-    res.send({
-        status: 'failed',
-        message: 'Resource ' + req.originalUrl + ' Not Found. '
-    });
-});
+
 app.listen(3000, () => console.log('Serever: http://localhost:3000'));
